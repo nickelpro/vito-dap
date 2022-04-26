c2bce818b1e11d5ab7ec91f5223d39ae0e3f9638
Vito Gamberini <vito@gamberini.email>
2022-04-26 10:28:06 -0400

    Maybe get all the cmake things working

---
+++ cmake/vdap-config.cmake
@@ -0,0 +1,2 @@
+find_package(nlohmann_json CONFIG REQUIRED)
+include(${CMAKE_CURRENT_LIST_DIR}/vdapTarget.cmake)
---
+++ test/test.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+#include <vdap/dap.hpp>
+
+int main() {
+  dap::ExceptionFilterOptions test;
+  std::cout << "Hello World\n";
+}
